2dd253be9a4e6246dacb4cf39a7607975eda9c43
champagnearden <jbbeck42@gmail.com>
2024-09-30 08:22:35 +0200

    replace moodle course id (ICA 4A)

--- background.js
+++ background.js
@@ -60,7 +60,8 @@ function retrieveCredentials() {
 
 async function loginAndFetch(username) {
     //const loginUrl = 'https://moodle.univ-ubs.fr/login/index.php';
-    const attendanceUrl = 'https://moodle.univ-ubs.fr/mod/attendance/view.php?id=421553';
+    // TODO: remplacer id du cours par id custom
+    const attendanceUrl = 'https://moodle.univ-ubs.fr/mod/attendance/view.php?id=433343';
 
     //const formData = new URLSearchParams();
     //formData.append('username', username);
